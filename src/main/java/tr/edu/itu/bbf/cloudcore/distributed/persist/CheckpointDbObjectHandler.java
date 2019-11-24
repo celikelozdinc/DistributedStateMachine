704a999c905b178364155fd2905f64d065085ab3
celikelozdinc <ozdinc.celikel@gmail.com>
2019-11-24 22:00:40 +0300

    checkpoint repository

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/persist/CheckpointDbObjectHandler.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/persist/CheckpointDbObjectHandler.java
@@ -17,117 +17,11 @@ import java.util.Optional;
 @Component
 public class CheckpointDbObjectHandler {
 
-    //@Autowired
-    //private CheckpointRepository checkpointRepository;
+    @Autowired
+    private CheckpointRepository checkpointRepository;
 
     // INSERT
     public CheckpointDbObject insertCheckpoint(@NotNull CheckpointDbObject checkpointDbObject){
-        CheckpointRepository checkpointRepository = new CheckpointRepository() {
-            @Override
-            public <S extends CheckpointDbObject> List<S> saveAll(Iterable<S> entities) {
-                return null;
-            }
-
-            @Override
-            public List<CheckpointDbObject> findAll() {
-                return null;
-            }
-
-            @Override
-            public List<CheckpointDbObject> findAll(Sort sort) {
-                return null;
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> S insert(S entity) {
-                return null;
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> List<S> insert(Iterable<S> entities) {
-                return null;
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> List<S> findAll(Example<S> example) {
-                return null;
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> List<S> findAll(Example<S> example, Sort sort) {
-                return null;
-            }
-
-            @Override
-            public Page<CheckpointDbObject> findAll(Pageable pageable) {
-                return null;
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> S save(S entity) {
-                return null;
-            }
-
-            @Override
-            public Optional<CheckpointDbObject> findById(Integer integer) {
-                return Optional.empty();
-            }
-
-            @Override
-            public boolean existsById(Integer integer) {
-                return false;
-            }
-
-            @Override
-            public Iterable<CheckpointDbObject> findAllById(Iterable<Integer> integers) {
-                return null;
-            }
-
-            @Override
-            public long count() {
-                return 0;
-            }
-
-            @Override
-            public void deleteById(Integer integer) {
-
-            }
-
-            @Override
-            public void delete(CheckpointDbObject entity) {
-
-            }
-
-            @Override
-            public void deleteAll(Iterable<? extends CheckpointDbObject> entities) {
-
-            }
-
-            @Override
-            public void deleteAll() {
-
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> Optional<S> findOne(Example<S> example) {
-                return Optional.empty();
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> Page<S> findAll(Example<S> example, Pageable pageable) {
-                return null;
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> long count(Example<S> example) {
-                return 0;
-            }
-
-            @Override
-            public <S extends CheckpointDbObject> boolean exists(Example<S> example) {
-                return false;
-            }
-        };
         System.out.println("INSERT CHECKPOINT");
         System.out.println("Timestamp inside db object ->" + checkpointDbObject.timestamp.toString());
         if (checkpointRepository == null) {System.out.println("Checkpoint repository is null.");}
