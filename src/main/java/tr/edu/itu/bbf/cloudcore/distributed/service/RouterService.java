aaaf40cae91355b40f3b7d098e15d90c7d2f05e3
celikelozdinc <ozdinc.celikel@gmail.com>
2019-11-24 19:50:47 +0300

    try to persist inside RouterService

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/service/RouterService.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/service/RouterService.java
@@ -1,12 +1,20 @@
 package tr.edu.itu.bbf.cloudcore.distributed.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tr.edu.itu.bbf.cloudcore.distributed.persist.CheckpointDbObject;
+import tr.edu.itu.bbf.cloudcore.distributed.persist.CheckpointDbObjectHandler;
 
 @Service
 public class RouterService {
 
+    @Autowired
+    private CheckpointDbObjectHandler dbObjectHandler;
+
     public void setCheckpoint(String context) {
-        System.out.printf("SMOC context is : %s\n",context);
+        System.out.printf("SMOC context inside RouterService : %s\n",context);
+        CheckpointDbObject dbObject = new CheckpointDbObject(1, context);
+        dbObjectHandler.insertCheckpoint(dbObject);
     }
 
     public String getCheckpoint(){return "N/A";}
