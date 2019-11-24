e0eefcfd15faf43d981633341c66d322e0df3f5c
celikelozdinc <ozdinc.celikel@gmail.com>
2019-11-24 17:42:32 +0300

    change from Component to Service

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/messaging/InputService.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/messaging/InputService.java
@@ -1,8 +1,9 @@
 package tr.edu.itu.bbf.cloudcore.distributed.messaging;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class InputService {
 
     public String ckpt(String context) {
--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/messaging/OutputService.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/messaging/OutputService.java
@@ -3,11 +3,12 @@ package tr.edu.itu.bbf.cloudcore.distributed.messaging;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import tr.edu.itu.bbf.cloudcore.distributed.persist.CheckpointDbObject;
 import tr.edu.itu.bbf.cloudcore.distributed.persist.CheckpointDbObjectHandler;
 import tr.edu.itu.bbf.cloudcore.distributed.persist.CheckpointRepository;
 
-@Component
+@Service
 public class OutputService {
 
     @Autowired
