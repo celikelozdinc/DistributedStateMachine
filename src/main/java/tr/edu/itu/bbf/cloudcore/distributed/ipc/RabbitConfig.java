59af4edda6ac7f1d3269ea5dc94a9a688a6c4827
celikelozdinc <ozdinc.celikel@gmail.com>
2019-12-30 18:18:38 +0300

    fix variables in compose

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
@@ -13,17 +13,12 @@ public class RabbitConfig {
     @Value("${EXCHANGE}")
     private String IPC_EXCHANGE;
 
-    @Value("${EVENT_QUEUE_SMOC2}")
-    private String EVENT_QUEUE_SMOC2;
+    @Value("${EVENT_QUEUE}")
+    private String EVENT_QUEUE;
 
-    @Value("${EVENT_EXCHANGE_SMOC2}")
-    private String EVENT_EXCHANGE_SMOC2;
+    @Value("${EVENT_EXCHANGE}")
+    private String EVENT_EXCHANGE;
 
-    @Value("${EVENT_QUEUE_SMOC1}")
-    private String EVENT_QUEUE_SMOC1;
-
-    @Value("${EVENT_EXCHANGE_SMOC1}")
-    private String EVENT_EXCHANGE_SMOC1;
 
 
     @Bean
@@ -42,25 +37,15 @@ public class RabbitConfig {
     }
 
     @Bean
-    Queue eventQueue(){ return new Queue(EVENT_QUEUE_SMOC2, false);}
+    Queue eventQueue(){ return new Queue(EVENT_QUEUE, false);}
 
     @Bean
-    DirectExchange eventExchange(){return new DirectExchange(EVENT_EXCHANGE_SMOC2);}
+    DirectExchange eventExchange(){return new DirectExchange(EVENT_EXCHANGE);}
 
     @Bean
     Binding bindingForEvent(Queue eventQueue, DirectExchange eventExchange){
         return BindingBuilder.bind(eventQueue).to(eventExchange).with("rpc");
     }
 
-    @Bean
-    Queue eventQueue_smoc1(){ return new Queue(EVENT_QUEUE_SMOC1, false);}
-
-    @Bean
-    DirectExchange eventExchange_smoc1(){return new DirectExchange(EVENT_EXCHANGE_SMOC1);}
-
-    @Bean
-    Binding bindingForEvent_smoc1(Queue eventQueue_smoc1, DirectExchange eventExchange_smoc1){
-        return BindingBuilder.bind(eventQueue_smoc1).to(eventExchange_smoc1).with("rpc");
-    }
 
 }
