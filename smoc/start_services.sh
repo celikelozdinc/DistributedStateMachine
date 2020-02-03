9322c1aa5ea5300b6e1d383f5062803c3d575c03
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-03 19:46:03 +0300

    change start_services script

--- smoc/start_services.sh
+++ smoc/start_services.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "Starting mongodb database service..."
+/usr/bin/mongod --bind_ip 0.0.0.0 ;
+echo "Starting statemachine jar and redirecting the output to log file..."
 /usr/bin/java -Dtimesleep=1000 -jar DistributedStateMachine_jar/DistributedStateMachine.jar > log
