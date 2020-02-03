05a290ea4a0a31ff19aeb41153ef4aa63ced5e32
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-03 20:22:29 +0300

    RUN mongod

--- smoc/start_services.sh
+++ smoc/start_services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting mongodb database service..."
-/usr/bin/mongod --bind_ip 0.0.0.0 ;
+/usr/bin/mongod --fork --logpath /var/log/mongod.log --bind_ip 0.0.0.0 > /dev/null
 echo "Starting statemachine jar and redirecting the output to log file..."
 /usr/bin/java -Dtimesleep=1000 -jar DistributedStateMachine_jar/DistributedStateMachine.jar > log
