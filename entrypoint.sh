f8ab4ba2344b01912bf8416219f5252771dbb9dc
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-03 19:27:57 +0300

    fix for grep

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Opening new terminal inside "$1"..."
-docker exec -it $(docker ps | grep distributedstatemachine_"$1" | awk '{print $1}') /bin/sh
+docker exec -it $(docker ps | grep -w distributedstatemachine_"$1" | awk '{print $1}') /bin/sh
