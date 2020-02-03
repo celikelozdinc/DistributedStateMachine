62645461b3e6a66e0204b2d07b3fae6984cd3085
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-03 19:27:11 +0300

    fix for searching smoc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Opening new terminal inside "$1"..."
-docker exec -it $(docker ps | grep $1 | awk '{print $1}') /bin/sh
+docker exec -it $(docker ps | grep distributedstatemachine_"$1" | awk '{print $1}') /bin/sh
