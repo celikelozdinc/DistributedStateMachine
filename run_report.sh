1a4c208180c0260b81d55b9df00fd29e6afa7d48
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-04 09:04:40 +0300

    fix for var

--- run_report.sh
+++ run_report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Getting container id ..."
 container_id=$(docker ps | grep -w distributedstatemachine_"$1" | awk '{print $1}')
-echo "Runnig report inside container ..."
-docker exec -it "container_id" ./report.sh
+echo "Runnig report inside container ""$container_id"
+docker exec -it "$container_id" ./report.sh
