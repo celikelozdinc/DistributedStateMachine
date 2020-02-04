a4baba324248fdaddfa9c3b579a5f25d2a22c1c5
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-04 21:41:01 +0300

    refactor for aggregated report

--- run_report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Getting container id ..."
-container_id=$(docker ps | grep -w distributedstatemachine_"$1" | awk '{print $1}')
-echo "Runnig report inside container ""$container_id"
-docker exec -it "$container_id" ./report.sh
