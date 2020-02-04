45b669d3cff59eaa0500f14cd27b291950f5de1d
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-04 21:43:21 +0300

    fix for aggregated report

--- aggregated_report.sh
+++ aggregated_report.sh
@@ -3,8 +3,7 @@ smocs=("smoc1" "smoc2" "smoc3" "smoc4" "smoc5" "smoc6" "smoc7" "smoc8" "smoc9" "
 for elem in "${smocs[@]}";
 do
 container_id=$(docker ps | grep -w distributedstatemachine_"$elem" | awk '{print $1}')
-if [[ -n "$container_id" ]];
-then docker exec -it "$container_id" ./report.sh;
+if [[ -n "$container_id" ]]; then docker exec -it "$container_id" ./report.sh;
 else echo "distributedstatemachine_""$elem"" : no such container";
 fi;
 done
