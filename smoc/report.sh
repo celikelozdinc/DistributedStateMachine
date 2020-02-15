32335eda6c5c9965ac5de551ebc8643991012b6e
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-15 13:31:44 +0300

    fix report.sh

--- smoc/report.sh
+++ smoc/report.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 # Duration in order to up a smoc
-jvm=$(grep "Started" log | awk -F 'in | seconds' '{print $2}')
-#echo "Sum for duration is ""$jvm"
+if [[ "${HOSTNAME}" == smoc4 ]]; then
+  jvm=$(grep "Started" log | awk -F 'in | seconds' '{print $2}')
+  ckpt=$(grep "Applied" log | awk -F 'in | seconds' '{print $2}')
+  sum=$(echo "$jvm" + "$ckpt" | bc)
+else
+  sum=$(grep "Started" log | awk -F 'in | seconds' '{print $2}')
+fi
 
 # Learn PID
 # xargs will do trimming
-current_pid=$(grep "PID" log | awk -F "PID@HOSTNAME is " '{print $2}' | cut -d'@' -f1 | xargs)
-#echo "PID is ""$current_pid"
+current_pid=$(grep "PID" log | awk -F "INFO|---" '{print $2}' | xargs)
+#current_pid=$(grep "PID" log | awk -F "PID@HOSTNAME is " '{print $2}' | xargs)
 
 # Memory Report from /proc/<pid>/status
 # xargs will do trimming
 VmSize=$(grep "VmSize" /proc/"$current_pid"/status | awk -F 'VmSize:|kB' '{print $2}' | xargs)
 VmPeak=$(grep "VmPeak" /proc/"$current_pid"/status | awk -F 'VmPeak:|kB' '{print $2}' | xargs)
-#echo "VmPeak is ""$VmPeak"
-#echo "VmSize is ""$VmSize"
 
 # Memory Report from top command
 # -v for awk : define variable
 # -n for top : exit after n iteration
 from_top=$(top -n 1 | awk -v search="$current_pid" '$1 == search {print $5}' | cut -d'm' -f1)
-#echo "Current memory consumption is ""$from_top"
 
 #echo "Measures in CSV format:"
-echo "$jvm","$VmPeak","$VmSize","$from_top"
+echo "$sum","$VmPeak","$VmSize","$from_top"
