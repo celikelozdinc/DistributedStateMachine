79d47e1e24093d4e009cab5797b348e9d893f90e
celikelozdinc <ozdinc.celikel@gmail.com>
2020-02-21 18:05:27 +0300

    #smocs=11 with zk for #events=300

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
@@ -80,7 +80,7 @@ public class Application implements CommandLineRunner {
 
         /* Read hostname from ENV of SMOC */
         String hostname = System.getenv("HOSTNAME");
-        if (hostname.equals("smoc12")){
+        if (hostname.equals("smoc20000000000")){
             logger.warn("{} read CKPTs from other smocs...",hostname);
             /* Read CKPT information from other smocs */
 
